ddc678c49386de626e10f9db797adf89125b2d60
Hazelnutsu <alonsegev05@gmail.com>
2025-04-07 12:19:32 +0300

    Add project files.

---
+++ Maman 12/DaryHeap.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Maman_12
+{
+    internal class DaryHeap
+    {
+        private List<int> _input;
+        private int _d; //number of sons
+
+
+
+
+        public DaryHeap(int d, List<int> intArray)
+        {
+            _d = d;
+            _input = intArray;
+        }
+
+        public void BuildHeap()
+        {
+
+        }
+
+        
+        public void Change_d(int d)
+        {
+
+        }
+
+        public void ExtractMax()
+        {
+
+        }
+        
+        public void InsertX(int x)
+        {
+
+        }
+
+        public void PrintHeap()
+        {
+
+        }
+        private void Max_Heapify(int i)
+        {
+
+        }
+
+        //returns the parent of a certain son indexed i
+        private int Parent(int i)
+        {
+            double a = i / _d;
+           
+            return (int)Math.Floor(a);
+        }
+        
+        //returns the k-th son of a certain node indexed i
+        private int D_son(int i, int k)
+        {
+            return _d * i - _d + k + 1;
+        }
+
+
+
+    }
+}
---
+++ Maman 12/Maman12.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maman_12
+{
+    internal class Maman12
+    {
+        public static void Main(string[] args)
+        {
+            string s1 = "Please pick one of the following options:" +
+                "\n 1. Build heap" +
+                "\n 2. Change d" +
+                "\n 3. Extract max" +
+                "\n 4. Insert" +
+                "\n 5. Print heap" +
+                "\n 6. Exit";
+
+            bool flag = true;
+
+            DaryHeap place_holderHeap;
+
+            Console.WriteLine(s1);
+
+            while (flag)
+            {
+               
+
+                char inputKey = Console.ReadKey().KeyChar;
+                
+
+                switch (inputKey)
+                {
+                    case '1':
+                        //build heap function call
+                        Console.Clear();
+                        Console.WriteLine("\n Enter values for the heap: ");
+                        string inputString = Console.ReadLine();
+                        Console.WriteLine("Enter the d value: ");
+                        int inputD = int.Parse(Console.ReadLine());
+                        string[] inputStringArray = inputString.Split(' ');
+
+                        List<int> intArray = new List<int>();
+                        foreach(string num in inputStringArray)
+                        {
+                            int convertedNum = int.Parse(num);
+                            intArray.Add(convertedNum);
+
+                        }
+                        place_holderHeap = new DaryHeap(inputD, intArray);
+
+
+                        break;
+
+                    case '2':
+                        //change d function call
+                        Console.WriteLine("Enter value: ");
+                        break;
+                    case '3':
+                    //extract max function call
+
+                    case '4':
+                        //insert node function call
+                        Console.WriteLine("Enter value: ");
+                        break;
+                    case '5':
+                        //print heap function call
+
+                    case '6':
+                        //exit program
+                        flag = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Please enter a valid number");
+                        break;
+
+                }
+                Console.WriteLine(s1);
+            }
+
+
+        }
+         
+    }
+    
+    
+}
